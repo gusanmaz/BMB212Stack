0beb87231936859f232a89e831521d022f2deb82
Güvenç Usanmaz <gusanmaz@gmail.com>
2022-04-26 09:48:57 +0300

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,34 @@
+public class Stack {
+    public char[] s;
+    public int N;
+
+    public Stack(int cap) {
+        s = new char[cap];
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public boolean isEmpty(){
+        return N == 0;
+    }
+
+
+    public void push(char item) {
+        s[N++] = item;
+    }
+
+    public char pop() {
+        return s[--N];
+    }
+    
+    
+    // Bu fonksiyon parametre olarak aldigi iki yiginin icerigi ayni ise true aksi halde false degerini dondurur.
+    // Iki yiginda bos ise bu fonksiyon true degerini dondurur.
+    // Iki yiginin da icerigi ayni fakat kapasiteleri farkli ise (s dizisinin eleman sayilari farkli ise) fonksiyon true degerini dondurur.
+    // Fonksiyon taniminda (function signature) herhangi bir degisiklik yapmayiniz.
+    public static boolean Equal(Stack s1, Stack s2){
+        
+    }
+}
